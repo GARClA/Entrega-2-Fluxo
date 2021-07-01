f88a21f2f12412258514c209c6e113ad9ef8ddc5
LGarcia <lucasgsa02@gmail.com>
2021-07-01 02:07:22 -0300

    Features de adicionar elementos as turmas adicionadas

--- objetos.py
+++ objetos.py
@@ -14,3 +14,11 @@ class Aluno:
         self._primeiro_nome = _primeiro_nome
         self._sobrenome = _sobrenome
         self._cpf = _cpf
+
+class Turma:
+    def __init__(self, _nome_turma, _materia, _professor, _alunos, _notas_alunos):
+        self._nome_turma = _nome_turma
+        self._materia = _materia
+        self._professor = _professor
+        self._alunos = _alunos
+        self._notas_alunos = _notas_alunos
--- sistema.py
+++ sistema.py
@@ -2,12 +2,15 @@
 materias = []
 professores = []
 alunos = []
+turmas = []
 
-#Importando os objetos
+lista_vazia = []
+
+#Importando os objetos e bibliotecas
 from objetos import *
+import random
 
-#Criando as funcionalidades
-    #Do menu principal
+#Criando as funcionalidades do menu principal
 def cadastrar_aluno():
     primeiro_nome = input("Digite o primeiro nome do aluno: ")
     sobrenome = input("Digite o sobrenome do aluno: ")
@@ -53,6 +56,42 @@ def mostrar_todas_materias():
         i += 1
     menu_principal()
 
+#Criando as funcionalidades do menu de turmas
+def cadastrar_turma():
+    materia = input("Insira o nome da matéria da turma: ")
+    nome_da_turma = f"{materia}{random.randint(1000,9999)}"
+    turmas.append(Turma(nome_da_turma,materia,None,lista_vazia,None))
+    print(f"A turma {nome_da_turma} está pronta para receber os professores e os alunos")
+    menu_turmas()
+
+def designar_professor():
+    turma_selecionada = input("Insira o nome da turma: ")
+    turmas_atuais = []
+    for turma in turmas:
+        turmas_atuais.append(turma._nome_turma)
+    if turma_selecionada in turmas_atuais:
+        professor = input("Insira o nome do professor da turma: ")
+        turma._professor = professor
+        print(f"O professor {professor} foi designado para a turma {turma._nome_turma}")
+        menu_turmas()
+    else:
+        print("Turma não encontrada")
+        menu_turmas()
+
+def designar_aluno():
+    turma_selecionada = input("Insira o nome da turma: ")
+    turmas_atuais = []
+    for turma in turmas:
+        turmas_atuais.append(turma._nome_turma)
+    if turma_selecionada in turmas_atuais:
+        aluno = input("Insira o nome do aluno: ")
+        turma._alunos.append(aluno)
+        print(f"O aluno {aluno} foi designado para a turma {turma._nome_turma}")
+        menu_turmas()
+    else:
+        print("Turma não encontrada")
+        menu_turmas()
+
 #Criando os menus
 def menu_principal():
     print("[1] - Cadastrar Aluno \n[2] - Cadastrar Novo Professor \n[3] - Cadastrar Nova Matéria \n[4] - Mostrar Todos os Alunos")
@@ -78,7 +117,20 @@ def menu_principal():
         print("Opção não encontrada")
 
 def menu_turmas():
-    print("Menu em construção")
+    print("[1] - Cadastrar Nova Turma \n[2] - Designar Professor Para Turma \n[3] - Atribuir Alunos a Turma")
+    print("[8] - Voltar ao Menu Principal")
+    opcao = input("Digite o número correspondente a opção desejada: ")
+    if int(opcao) == 1:
+        cadastrar_turma()
+    elif int(opcao) == 2:
+        designar_professor()
+    elif int(opcao) == 3:
+        designar_aluno()
+    elif int(opcao) == 8:
+        menu_principal()
+    else:
+        print("Opção não encontrada")
+
 
 #Executando o código
 menu_principal()
